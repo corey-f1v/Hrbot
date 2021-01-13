37b6301996d86c635e29f9db117d35326f364cae
corey-f1v <47674179+corey-f1v@users.noreply.github.com>
2021-01-13 13:23:34 -0500

    Hope this works (#8)
    Co-authored-by: Glitch (hello-express) <none>

--- gCal.js
+++ gCal.js
@@ -5,13 +5,13 @@ const {
 const calendarId =
 	"f1v.co_tthcm1hbtv21rlpaso7pliirmo@group.calendar.google.com";
 
-async function createEvent(summary, auth) {
+async function createEvent(summary, date, auth) {
 	const event = {
 		end: {
-			date: "2021-01-14"
+			date
 		},
 		start: {
-			date: "2021-01-14"
+			date
 		},
 		description: "-",
 		summary
@@ -37,15 +37,74 @@ async function createEvent(summary, auth) {
 	);
 }
 
-function createSickDayEvent(username, auth) {
-	createEvent(`${username} - OOO - Sick`, auth);
+async function createManyEvent(summary, start, end, description, auth) {
+	const event = {
+		end: {
+			date: `${end}`,
+		},
+		start: {
+			date: `${start}`,
+		},
+		description: `${description}`,
+		summary
+	};
+	const calendar = google.calendar({
+		version: "v3",
+		auth
+	});
+	calendar.events.insert({
+			auth,
+			calendarId,
+			resource: event,
+		},
+		function (err, event) {
+			if (err) {
+				console.log(
+					"There was an error contacting the Calendar service: " + err
+				);
+				return;
+			}
+		}
+	);
 }
 
-function createVacationDayEvent(username, auth) {
-	createEvent(`${username} - OOO - Vacation`, auth);
+function getDate() {
+	var today = new Date();
+	var dd = today.getDate();
+
+	var mm = today.getMonth() + 1;
+	var yyyy = today.getFullYear();
+	if (dd < 10) {
+		dd = "0" + dd;
+	}
+
+	if (mm < 10) {
+		mm = "0" + mm;
+	}
+	today = yyyy + "-" + mm + "-" + dd;
+	return today;
+}
+
+function createSickDayEvent(username, date, auth) {
+	createEvent(`${username} - OOO - Sick`, date, auth);
+}
+
+function createManySickDayEvent(username, start, end, auth) {
+	createManyEvent(`${username} - OOO - Sick`, start, end, '-', auth);
+}
+
+function createVacationDayEvent(username, date, auth) {
+	createEvent(`${username} - OOO - Vacation`, date, auth);
+}
+
+function createManyVacationDayEvent(username, start, end, auth) {
+	createManyEvent(`${username} - OOO - Vacation`, start, end, '-', auth);
 }
 
 module.exports = {
 	createSickDayEvent,
-	createVacationDayEvent
+	createVacationDayEvent,
+	getDate,
+	createManySickDayEvent,
+	createManyVacationDayEvent
 }
